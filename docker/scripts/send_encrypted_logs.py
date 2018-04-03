f57364e561985b849c215038cad27761c68adb52
Jivan Amara <Development@JivanAmara.net>
2018-04-03 13:54:38 -0600

    add executable permission for init & cron scripts

--- docker/scripts/send_encrypted_logs.py
+++ docker/scripts/send_encrypted_logs.py
@@ -14,8 +14,8 @@ from meditate.orders import send_alert
 MAIL_USER = os.environ.get('MAIL_USER', '')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '')
 
-encrypt_gunicorn_log = 'cat /var/log/gunicorn_meditate.log | gpg -a -e --default-recipient 0x05B29E8D'
-encrypt_nginx_log = 'cat /var/log/nginx/error.log | gpg -a -e --default-recipient 0x05B29E8D'
+encrypt_gunicorn_log = 'cat /var/log/gunicorn_meditate.log | gpg -a -e --always-trust -r 0x05B29E8D'
+encrypt_nginx_log = 'cat /var/log/nginx/error.log | gpg -a -e --always-trust -r 0x05B29E8D'
 gunicorn_log = subprocess.check_output(['/bin/bash', '-c', encrypt_gunicorn_log]).decode('utf-8')
 nginx_log = subprocess.check_output(['/bin/bash', '-c', encrypt_nginx_log]).decode('utf-8')
 
